5d29ca19cbaac18ef6cd1e01379cca9219c08994
wang zongwen <wangzw21@lenovo.com>
2021-02-09 04:55:43 +0800

    修改spark read hdfs代码

---
+++ Spark/src/main/scala/com.wangzhe.spark/SparkConnJdbc.scala
@@ -0,0 +1,8 @@
+package com.wangzhe.spark
+
+object SparkConnJdbc {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+}
--- Spark/src/main/scala/com.wangzhe.spark/SparkReadHDFS.scala
+++ Spark/src/main/scala/com.wangzhe.spark/SparkReadHDFS.scala
@@ -1,14 +1,22 @@
 package com.wangzhe.spark
 
+import java.io.File
+
+import org.apache.hadoop.io.{LongWritable, Text}
+import org.apache.hadoop.mapred.TextInputFormat
 import org.apache.spark.{SparkConf, SparkContext}
 
 
 object SparkReadHDFS {
-
   def main(args: Array[String]): Unit = {
-    val sparkConf = new SparkConf()
-    val sc = SparkContext.getOrCreate(sparkConf)
-
-    sc.hadoopFile("/tmp").map(s => s + "sdfs")
+    val jars = new File("/opt/spark-2.3.2-bin-hadoop2.6/jars")
+      .list().map(name => "/opt/spark-2.3.2-bin-hadoop2.6/jars/" + name).mkString(",")
+    System.out.println(jars)
+    val conf = new SparkConf().setMaster("yarn").setAppName("readHdfs")
+      .setSparkHome("/usr/lib/spark/jars")
+      .set("spark.yarn.jars", jars)
+      //.setJars(Seq("Spark/target/Spark-1.0.jar"))
+    val sc = new SparkContext(conf)
+    sc.hadoopFile[LongWritable, Text, TextInputFormat]("/test").map(s => (s, 1))
   }
 }
